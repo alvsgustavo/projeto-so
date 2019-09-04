ccf1033d687211c8755de43065bdb79ecf07d639
alvsgustavo <gustavo.daniel.alves@ccc.ufcg.edu.br>
2019-09-04 19:58:22 -0300

    regression

--- lab_shell/xeu.cpp
+++ lab_shell/xeu.cpp
@@ -2,7 +2,8 @@
 #include "unistd.h"
 #include "pwd.h"
 #include "sys/wait.h"
-#include <pthread.h>
+#include "pthread.h"
+#include "linux/limits.h"
 
 #include <iostream>
 #include <vector>
@@ -12,100 +13,52 @@
 using namespace xeu_utils;
 using namespace std;
 
-vector<struct thread_data*> bgp{};
-pthread_mutex_t lock;
-
-struct thread_data {
-  Command c;
-  pthread_t t;
-  pid_t pid;
-};
-
-
-void *run_bg(void *vargp) {
-  struct thread_data *args = (struct thread_data *) vargp;
-  pthread_t thread = args->t;
-  Command c = args->c;
-
-  pid_t pid = fork();
-
-  if (pid == -1) {
-    cout << "Failed to fork process" << endl;
-
-  } else if (pid == 0) {
-    if (execvp(c.filename(), c.argv()) == -1)
-      cout << "Error whilst trying to execute " << c.filename() << endl;
-
-  } else {
-    args->pid = pid;
-    cout << "[+] " << pid << endl;
-    pthread_mutex_lock(&lock);
-    bgp.push_back(args);
-    pthread_mutex_unlock(&lock);
-    waitpid(args->pid, NULL, 0);
-    pthread_mutex_lock(&lock);
-    for (int i = 0; i < bgp.size(); i++) {
-      struct thread_data proc = *(bgp.at(i));
-
-      if (proc.pid == pid && proc.c.name() == c.name()) {
-        bgp.erase(bgp.begin() + i);
-        break;    
-      }
-    }
-
-    pthread_mutex_unlock(&lock);
-  }
-}
-
-void run(const Command &c) {
+pid_t fork_and_exec(const Command &c) {
   pid_t pid = fork();
 
   if (pid == -1)
     cout << "Failed to fork process" << endl;
-  else if (pid == 0) 
-    if (execvp(c.filename(), c.argv()) == -1)
+  
+  else if (pid == 0 && execvp(c.filename(), c.argv()) == -1) {
       cout << "Error whilst trying to execute " << c.filename() << endl;
+  }
 
-  else
-    waitpid(pid, NULL, 0);
-    
+  return pid;
+}
+
+void run(Command &c) {
+  pid_t pid = fork_and_exec(c);
+  int status;
+
+  if (pid > 0)
+    waitpid(pid, &status, 0);
+}
+
+
+void run_bg(Command &c) {
+  
 }
 
 void parse_command(Command &c) {
-  if (c.name() == "cd")
-    chdir(c.argv()[1]);
-  else if (c.name() == "exit")
-    exit(EXIT_SUCCESS);
-  else if (c.name() == "xjobs") {
-    cout << "XJOBS" << endl;
-    pthread_mutex_lock(&lock);
-    for (int i = 0; i < bgp.size(); i++) {
-      struct thread_data p = *(bgp.at(i));
-      cout << p.pid << " " << p.c.name() << endl; 
-    }
-    pthread_mutex_unlock(&lock);
+  string command = c.name();
 
-  } else if (c.name() == "bg") {
-    struct thread_data vargp;
-    pthread_t thread_id;
-    vargp.c = c;
-    vargp.t = thread_id;
-    pthread_create(&thread_id, NULL, &run_bg, (void *)&vargp);
-    
-  } else
+  if (command == "cd")
+    chdir(c.argv()[1]);
+
+  else if (command == "exit")
+    exit(EXIT_SUCCESS);
+
+  else if (command == "bg")
+    run_bg(c);
+
+  else
     run(c);
 }
 
 void print_prompt() {
-  /*
-   * char* cwd;
-   * getcwd(cwd, sizeof(char) * 1024);
-   * 
-   * THE CODE ABOVE CAUSES A SEGMENTATION FAULT, BUT WHY?
-   */
-
-  std::cout << getcwd(NULL, sizeof(char) * 1024) << endl; // This one causes a memory leak.
-  std::cout << "$ ";
+  char path[PATH_MAX];
+  getcwd(path, sizeof(path));
+  cout << path << endl << "$ ";
 }
 
 int main() {
@@ -113,8 +66,8 @@ int main() {
     print_prompt();
     vector<Command> commands = StreamParser().parse().commands();
 
-    for (int i = 0; i < commands.size(); i++)
-      parse_command(commands[i]);
+    for (Command& c: commands)
+      parse_command(c);
     cout << endl;
   }
 
