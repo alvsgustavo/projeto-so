03797804c8d5625bf0cdb220c16b83c9facd5a01
marcosbmf <marcos.barros.filho@ccc.ufcg.edu.br>
2019-10-05 20:42:06 -0300

    NRU algorithm added

--- mem-shell/lab_mem/page_replacement/python/algorithms/nru.py
+++ mem-shell/lab_mem/page_replacement/python/algorithms/nru.py
@@ -1,3 +1,5 @@
+import random
+
 # This is the file where you must implement the NRU algorithm
 
 # This file will be imported from the main code. The PhysicalMemory class
@@ -7,19 +9,35 @@
 # NOTE: there may be methods you don't need to modify, you must decide what
 # you need...
 
+class Frame:
+  def __init__(self, frameId):
+    self.frameId = frameId
+    self.ref = 1
+    self.mod = 0
+
 class NRU:
 
   def __init__(self):
-    pass
+    self.list = []
 
   def put(self, frameId):
-    pass
+    self.list.append(Frame(frameId))
 
   def evict(self):
-    pass
-
+    classes = { 0: [], 1: [], 2: [], 3:[]}
+    for frame in self.list:
+      classes.get(frame.ref*2 + frame.mod).append(frame) ##Every frame goes into their class list.
+    for x in xrange(4):
+      if len(classes.get(x)) > 0:
+        deletedFrame = self.list.pop(self.list.index(random.choice(classes.get(x))))
+        return deletedFrame.frameId
+    
   def clock(self):
-    pass
+    for frame in self.list:
+      frame.ref = 0
 
   def access(self, frameId, isWrite):
-    pass
+    elem = [frame for frame in self.list if frame.frameId == frameId][0]
+    elem.ref = 1
+    if isWrite and elem.mod == 0: 
+      elem.mod = isWrite
