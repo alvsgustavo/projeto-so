d450a258676957d480c081014110607ca8ecbf05
marcosbmf <marcos.barros.filho@ccc.ufcg.edu.br>
2019-10-05 21:47:37 -0300

    Added aging algorithm

--- mem-shell/lab_mem/page_replacement/python/algorithms/aging.py
+++ mem-shell/lab_mem/page_replacement/python/algorithms/aging.py
@@ -10,19 +10,36 @@
 ALGORITHM_AGING_NBITS = 8
 """How many bits to use for the Aging algorithm"""
 
+class Frame:
+  def __init__(self, frameId):
+    self.frameId = frameId
+    self.age = 0
+    self.referenced = 1
+  
+  def aging(self):
+    self.age = (self.age >> 1) + self.referenced * 2**(ALGORITHM_AGING_NBITS-1)
+    self.referenced = 0
+
 class Aging:
 
   def __init__(self):
-    pass
+    self.list = []
 
   def put(self, frameId):
-    pass
+    self.list.append(Frame(frameId))
 
   def evict(self):
-    pass
+    minimumFrame = self.list[0]
+    for i in xrange(len(self.list)):
+      elem = self.list[i]
+      minimumFrame = elem if elem.age < minimumFrame.age else minimumFrame
+    self.list.remove(minimumFrame)
+    return minimumFrame.frameId
 
   def clock(self):
-    pass
+    for frame in self.list:
+      frame.aging()
 
   def access(self, frameId, isWrite):
-    pass
+    frame = [x for x in self.list if x.frameId == frameId][0]
+    frame.referenced = 1
